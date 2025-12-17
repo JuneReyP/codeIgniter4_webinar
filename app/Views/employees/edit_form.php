97b053e4bb4fb6f09362e0c09da3244357cd2423
JuneReyP <junereypalabrica@gmail.com>
2025-12-17 19:14:59 +0800

    Add debugbar and session files for employee management
    - Created a new debugbar JSON file to log request details for the employees endpoint.
    - Added session files to store session data for user interactions with the employees controller.

--- app/Views/employees/edit_form.php
+++ app/Views/employees/edit_form.php
@@ -10,27 +10,58 @@
         </div>
     <?php endif; ?>
 
-    <form action="<?= site_url('employees/update/'.$employee['id']) ?>" method="post">
-        <?= csrf_field() ?>
-        <Label>First Name</Label>
-        <input type="text" name="first_name" value="<?= esc(old('first_name', $employee['first_name'])) ?>" />
-        <br>
-        <br>
-        <label>Last Name</label>
-        <input type="text" name="last_name" value="<?= esc(old('last_name', $employee['last_name'])) ?>" />
-        <br>
-        <br>
-        <label>Middle Name</label>
-        <input type="text" name="middle_name" value="<?= esc(old('middle_name', $employee['middle_name'])) ?>" />
-        <br>
-        <br>
-        <label>Birthday</label>
-        <input type="date" name="birthday" value="<?= esc(old('birthday', $employee['birthday'])) ?>" />
-        <br>
-        <br>
-        <button type="submit">Update Employee</button>
-        <a href="<?=  site_url('employees') ?>">Cancel</a>
+
+
+    <form class="row g-3" action="<?= site_url('employees/update/'.$employee['id']) ?>" method="post">
+        <div class="col-md-4">
+            <label for="validationDefault01" class="form-label">First name</label>
+            <input type="text" class="form-control" id="validationDefault01"  name="first_name" value="<?= esc(old('first_name', $employee['first_name'])) ?>">
+        </div>
+        <div class="col-md-4">
+            <label for="validationDefault02" class="form-label">Last name</label>
+            <input type="text" class="form-control" id="validationDefault02" name="last_name" value="<?= esc(old('last_name', $employee['last_name'])) ?>">
+        </div>
+        <div class="col-md-4">
+            <label for="validationDefault03" class="form-label">Middle name</label>
+            <input type="text" class="form-control" id="validationDefault03" name="middle_name" value="<?= esc(old('middle_name', $employee['middle_name'])) ?>">
+        </div>        
+        <div class="col-md-4">
+            <label for="validationDefault03" class="form-label">Birthday</label>
+            <input type="date" class="form-control" id="validationDefault03" name="birthday" value="<?= esc(old('birthday', $employee['birthday'])) ?>">
+        </div>        
+        <div class="col-md-6">
+            <label for="gender" class="form-label">Gender</label>
+            <input type="text" class="form-control" id="gender" name="gender" value="<?= esc(old('gender', $employee['gender'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">Nationality</label>
+            <input type="text" class="form-control" id="validationDefault03" name="nationality" value="<?= esc(old('nationality', $employee['nationality'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">Height</label>
+            <input type="text" class="form-control" id="validationDefault03" name="height" value="<?= esc(old('height', $employee['height'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">Weight</label>
+            <input type="text" class="form-control" id="validationDefault03" name="weight" value="<?= esc(old('weight', $employee['weight'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">Mobile Number</label>
+            <input type="text" class="form-control" id="validationDefault03" name="mobile_number" value="<?= esc(old('mobile_number', $employee['mobile_number'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">Province</label>
+            <input type="text" class="form-control" id="validationDefault03" name="province" value="<?= esc(old('province', $employee['province'])) ?>">
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">City</label>
+            <input type="text" class="form-control" id="validationDefault03" name="city" value="<?= esc(old('city', $employee['city'])) ?>">
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Update Employee</button>
+        </div>
     </form>
+    
 </div>
 
 <?= $this->endSection() ?>
