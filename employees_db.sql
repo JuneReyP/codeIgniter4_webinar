6fedfa911263db782098d06cdd24b7fe82f7dd0b
JuneReyP <junereypalabrica@gmail.com>
2025-12-17 20:13:05 +0800

    Update employees database SQL dump with additional fields and corrected generation time

--- employees_db.sql
+++ employees_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2025 at 06:17 AM
+-- Generation Time: Dec 17, 2025 at 12:16 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,17 +32,22 @@ CREATE TABLE `employees` (
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `middle_name` varchar(100) DEFAULT NULL,
-  `birthday` date DEFAULT NULL
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(100) NOT NULL,
+  `nationality` varchar(100) NOT NULL,
+  `height` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  `mobile_number` int(11) NOT NULL,
+  `province` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employees`
 --
 
-INSERT INTO `employees` (`id`, `first_name`, `last_name`, `middle_name`, `birthday`) VALUES
-(1, 'Juan', 'Cruz', 'Lopez', '1999-05-09'),
-(2, 'June Rey', 'Palabrica', 'Lanohan', '2025-12-17'),
-(3, 'Maria', 'Jose', 'Juarez', '1989-04-04');
+INSERT INTO `employees` (`id`, `first_name`, `last_name`, `middle_name`, `birthday`, `gender`, `nationality`, `height`, `weight`, `mobile_number`, `province`, `city`) VALUES
+(1, 'test', 'test', 'test', '2025-12-16', 'adasdasd', 'asdasd', 123, 12, 1323213, 'dssd', 'Kabankalan');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +67,7 @@ ALTER TABLE `employees`
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
